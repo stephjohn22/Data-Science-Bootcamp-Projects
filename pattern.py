817d7208ab9bb8d35539bbbaf7318f9fa9048029
stephjohn22 <163042398+stephjohn22@users.noreply.github.com>
2024-08-01 20:26:48 +0100

    Delete pattern.py

--- pattern.py
+++
@@ -1,7 +0,0 @@
-middle_stars = 5
-# Prints out a pattern of stars where the middle line has 5 stars and it decreases on each line above and below the middle line
-for i in range(1, 2 * middle_stars):
-    if i <= middle_stars:
-        print("*" * i)
-    else:
-        print("*" * (2 * middle_stars - i))
